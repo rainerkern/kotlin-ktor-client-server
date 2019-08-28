f93a1163585f98da4b0ad7b3701a43893a1af804
rainer <rainer@systemkern.com>
2019-08-28 12:18:36 +0200

    Step 6: Add the Kotlin HTTP DSL and mainPage
    Adding the Kotlin HTTP Domain Specific Language, allows us to write type
    safe Kotlin code which generates valid HTML.

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,22 +1,35 @@
 package at.rainerkern.ktor
 
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
-import io.ktor.http.ContentType.Text
-import io.ktor.response.respondText
+import io.ktor.html.respondHtml
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.html.ATarget
+import kotlinx.html.a
+import kotlinx.html.body
+import kotlinx.html.div
+
 
 fun main() {
     embeddedServer(Netty, 8080) {
         routing {
             get("/") {
-                call.respondText(
-                    text = "Hello World",
-                    contentType = Text.Html
-                )
+                mainPage(call)
             }
         }
     }.start(wait = true)
+}
+
+suspend fun mainPage(call: ApplicationCall) = call.respondHtml {
+    body {
+        div {
+            a("https://kotlinlang.org") {
+                target = ATarget.blank
+                +"Main site"
+            }
+        }
+    }
 }
