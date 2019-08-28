ca54c01305d52070625305ee3973f0eee5a438d8
rainer <rainer@systemkern.com>
2019-08-28 12:15:57 +0200

    Step 5: Add netty HTTP server
    We add the dependencies for Ktor and Netty. This gives us the necessary
    tools to develop an actual server application.
    
    After starting the server it is reachable at http://localhost:8080

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,22 @@
+package at.rainerkern.ktor
+
+import io.ktor.application.call
+import io.ktor.http.ContentType.Text
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText(
+                    text = "Hello World",
+                    contentType = Text.Html
+                )
+            }
+        }
+    }.start(wait = true)
+}
